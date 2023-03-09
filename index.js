317d5ad682138f0d4b9c54d1d48cf6437273ac15
Jaiden Shahi <23jshahi@gmail.com>
2023-03-08 18:29:35 -0800

    Basic Functionality

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const fs = require('fs');
+
+// Discord.js versions ^13.0 require us to explicitly define client intents
+const { Client, GatewayIntentBits } = require('discord.js')
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+})
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async message => {
+    if (message.reference != null) {
+        const repliedTo = await message.channel.messages.fetch(message.reference.messageId);
+        console.log(message.reference)
+        console.log(repliedTo.content, message.content);
+        fs.readFile()
+
+        fs.appendFile('./data.csv', `"${repliedTo.content}","${message.content}"` + '\n', (err) => {
+            if (err) throw err;
+        });
+
+    }
+
+
+});
+
+// create an object to write to the file
+client.login(process.env.CLIENT_TOKEN);
